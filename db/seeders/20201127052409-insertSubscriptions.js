15c3c85d24a68c0efc73fa77eb2a500463a50bf7
chrisoney <christopher.oney@yahoo.com>
2021-02-22 16:08:37 -0800

    updating subscription with comments

--- db/seeders/20201127052409-insertSubscriptions.js
+++ db/seeders/20201127052409-insertSubscriptions.js
@@ -10,15 +10,6 @@ const { Op } = require('../models').Sequelize;
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-
       {
         book:'0',
         chapter:'',
@@ -32,7 +23,6 @@ module.exports = {
     // let values = tempSubs;
     let values = currentSubs;
     // Grabbing all of the existing recommendations for users that aren't my own account
-
     let oldArray = await Recommendation.findAll({
       where: {
         userId: {
@@ -42,10 +32,15 @@ module.exports = {
     })
     // Using the recommendations to create subscriptions that match
     for (let i = 0; i < oldArray.length; i++){
+      // We're going based off the recommendations so this just grabs each one
+      // after I queried for it
       let oldObj = oldArray[i];
       values.push({
+        // Completely random book
         book: `${Math.floor(Math.random() * 5)}`,
+        // Completely random chapter
         chapter: `${Math.floor(Math.random() * 30)}`,
+        // We want these last two to be the same as what is in the recommendation
         userId: oldObj.userId,
         storyId: oldObj.storyId,
       })
