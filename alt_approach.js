12d48596eb3de3dd6fb35732e6196438ccadbaec
chrisoney <christopher.oney@yahoo.com>
2021-02-27 00:00:00 -0800

    start alternate approach with follows build

---
+++ alt_approach.js
@@ -0,0 +1,22 @@
+const userStart = 1;
+const storyStart = 1;
+
+const altFollows = [
+  {
+    followerId: userStart,
+    followingId: userStart + 1
+  },
+  {
+    followerId: userStart,
+    followingId: userStart + 2
+  },
+  {
+    followerId: userStart,
+    followingId: userStart + 3
+  },
+];
+
+
+module.exports = {
+  altFollows
+}
