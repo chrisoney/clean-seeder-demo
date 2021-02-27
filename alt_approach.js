719a2f0236ae7b84de0acfe6d5673cc1c5b0e550
chrisoney <christopher.oney@yahoo.com>
2021-02-27 00:06:36 -0800

    add recommendation alternate to file

--- alt_approach.js
+++ alt_approach.js
@@ -1,3 +1,5 @@
+const { restart } = require("nodemon");
+
 const userStart = 1;
 const storyStart = 1;
 
@@ -17,6 +19,167 @@ const altFollows = [
 ];
 
 
+const altRecs = [
+  {
+    rating: 4,
+    review: 'Long time between chapters but the world building is top notch',
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: 'Fantastic story, would always recommend.',
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: 'One of my favorites',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'The writing is a bit verbose, but the story is interesting.',
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: 'One of my favorite ongoing stories',
+    userId: userStart,
+  },
+  {
+    rating: 3,
+    review: 'The writer puts out a ton of great content, but the MC is kinda cringe.',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Long hiatus but a really funny, dark story',
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: 'Fantastic worldbuilding even if the MC is annoying',
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: `I don't really like space stories but the characters and worldbuilding more and make up for it not being my preference.`,
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: `Really interesting worldbuilding. I can't wait until it 
+    starts up again`,
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: `Interesting twists. Unfortunately not being released 
+    freely right now`,
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: 'Absolutely hilarious and the world building is pretty impressive',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: `Overpowered MC but I think it addresses it rather well. I wish they would all stop smirking though.`,
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: `Short story but pretty good`,
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: `Part of the story are really cringey but overall it's entertaining`,
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Childish but funny',
+    userId: userStart,
+  },
+  {
+    rating: 3,
+    review: 'The ending of book one leaves much to be desired',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Great writing style. Sometimes it was rather slow-paced',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Surprisingly entertaining story about an ant',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Long hiatus. I hope it comes back soon',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Funny take on quests and fate',
+    userId: userStart,
+  },
+  {
+    rating: 3,
+    review: 'Interesting magic system, though pretty slow story progression',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: `Long hiatus, but it should be starting again soon. A more realistic approach to isekai`,
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Not a huge fan of pirate stories but this one won me over',
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: `Another story that isn't my usual cup of tea, but the writing is incredible and the characters are all likeable or well thought out`,
+    userId: userStart,
+  },
+  {
+    rating: 1,
+    review: 'Had a great start but it was cancelled far too early.',
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: `Hilarious and great world-building. I hope it the story doesn't run out of steam`,
+    userId: userStart,
+  },
+  {
+    rating: 5,
+    review: `Hilarious and it makes the MC work for his power`,
+    userId: userStart,
+  },
+  {
+    rating: 4,
+    review: 'Cool concept but it moves rather slowly',
+    userId: userStart,
+  },
+]
+
+const addStoryIdToRecs = () => {
+  const result = [];
+  for (let i = 0; i < altRecs.length; i++){
+    let temp = altRecs[i];
+    temp.storyId = storyStart + i;
+    result.push(temp);
+  }
+  return result;
+}
+
 module.exports = {
-  altFollows
+  altFollows,
+  addStoryIdToRecs,
 }
