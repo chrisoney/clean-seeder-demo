cf1fd4b1423026b220322dec3db72343ab221c21
chrisoney <christopher.oney@yahoo.com>
2021-03-04 21:02:40 -0800

    add comments to recommendations

--- db/seeders/20201127052408-insertRecommendations.js
+++ db/seeders/20201127052408-insertRecommendations.js
@@ -7,56 +7,68 @@ const { User, Story } = require('../models')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // Pulling in the recommendations that I wrote myself
+    // Step 1
+    // Initializing the array that I will pass into the bulk insert
     let values = [];
 
+    // Option 1
     // Dynamic approach to adding starting recommendations
+    // Start with the data I pulled in from the other file
     const startingRecs = currentRecs;
+    // Looping through each object in that prebuilt array
     for (let x = 0; x < startingRecs.length; x++) {
       let rec = startingRecs[x];
+      // Creating a new recommendation with the attributes that won't change
       const newRec = { rating: rec.rating, review: rec.review };
+      // Querying for the user and using the id for my new rec
       const user = await User.findOne({ where: { username: rec.user } });
       newRec.userId = user.id;
+      // Querying for the story and using the id for my new rec
       const story = await Story.findOne({ where: { title: rec.story } });
       newRec.storyId = story.id;
+      // Adding this new rec to my existing values
       values.push(newRec);
     }
 
+    // Option 2
     // Alt approach to adding starting recommendations
     // values = addStoryIdToRecs();
 
+    // Step 2
     // Making new recommendations with the number of reviews per user,
-    // number of users I have, number of stories I have, fake reviews, and id to start at
+    // number of users I have, number of stories I have, fake reviews, and id to start at for both users and stories
     function makeRecommendations(
-      number,
-      numUsers,
-      story,
+      numReviews,
+      userEnd,
+      numStories,
       reviews,
       userStart,
       storyStart
     ) {
+      // Initializing and empty array to hold them
       const result = [];
-      // Looping through all the users except the first one to add reviews for
-      // them
-      for (let k = userStart; k < numUsers; k++) {
+      // Looping through all the users except the first one to add reviews for them
+      for (let k = userStart; k < userEnd; k++) {
         // Keeping track of which stories this user has reviewed
         let stories = [];
-        for (let i = 0; i < number; i++) {
+        for (let i = 0; i < numReviews; i++) {
           // Grabbing a random review index
-          let spot = Math.floor(Math.random() * reviews.length);
-          // Grabbing a random story
-          let storyId = Math.floor(Math.random() * story) + storyStart;
+          let index = Math.floor(Math.random() * reviews.length);
+          // Grabbing a random storyId from the ones that are available
+          let storyId = Math.floor(Math.random() * numStories) + storyStart;
           // Making sure this user hasn't reviewed this story yet
           while (stories.includes(storyId))
-            storyId = Math.floor(Math.random() * story) + storyStart;
-          // Adding the id of the story so we can check against it later on
-          // when randomly grabbing a story id
+            storyId = Math.floor(Math.random() * numStories) + storyStart;
+          
+          // Adding the id of the story so we can check against it later on when randomly grabbing a story id
           stories.push(storyId);
+          
+          // Adding this new review to the results I have collected
           result.push({
             // completely random rating
             rating: Math.floor(Math.random() * 5) + 1,
             // Grabbing the review with the index we generated a few lines ago
-            review: reviews[spot],
+            review: reviews[index],
             userId: k,
             storyId,
           });
@@ -64,6 +76,8 @@ module.exports = {
       }
       return result;
     }
+
+    // Step 2 preparation
     // Fake reviews
     const reviews = [
       'Great!',
@@ -81,30 +95,41 @@ module.exports = {
       `I highly recommend this story to everybody. Even if you don't think you'll like it from what you hear. Not only does this story dodge every single pitfall that comes its way, it absolutely obliterates them.`,
       `This is absolutely terrible`,
     ];
-    // Querying to figure out how many users and stories I've seeded dynamically
+    // Querying to figure out how many users I've seeded already
     const numUsers = await User.count();
+    // Querying to figure out how many stories I've seeded already
     const numStories = await Story.count();
-    const numReviews = 5;
+    // Choosing how many reviews each user should have
+    const numRecs = 5;
+    // Finding the first user in my database and grabbing the id, then adding one because I don't want to add reviews to 'chris'
     let userStart = await User.findOne();
     userStart = userStart.id + 1;
+    // Finding the first story in my database and grabbing the id
     let storyStart = await Story.findOne();
     storyStart = storyStart.id;
-    // Grabbing those new fake recommendations. 5 is the number of recommendations
-    // per user. numUsers and numStories are self explanatory. Reviews are just
-    // a few fake reviews I created/stole from a website (I'm sure you can tell
-    // which is which). Fun idea: make different tiers of reviews you can choose
-    // from based on a rating!
+
+    // Step 2 implementation 
+    // Creating those new fake recommendations.
+    // numRecs is the number of recommendations per user.
+    // numUsers and numStories are self explanatory.
+    // Reviews are just a few fake reviews I created/stole from a website (I'm sure you can tell which is which). Fun idea: make different tiers of reviews you can choose from based on a rating!
+    // userStart is to determine the id I would start at
+    // storyStart is to determine the id I would start at
     let recArray = makeRecommendations(
-      numReviews,
+      numRecs,
+      // This is more of a me problem because I artificially inflated the starting point, so I have to correct for that when determining where to stop
       numUsers + userStart - 1,
       numStories,
       reviews,
       userStart,
       storyStart
     );
+
+    // Step 3
     // Combining what I wrote myself with what I used the function to create
     values.push(...recArray);
 
+    // Step 4
     // The actual seeding
     return queryInterface.bulkInsert('Recommendations', values, {});
   },
