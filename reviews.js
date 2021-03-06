2be0d9f94667238f1c66773f747f0136cc4a9943
chrisoney <christopher.oney@yahoo.com>
2021-03-05 20:27:51 -0800

    start dynamic reviews

---
+++ reviews.js
@@ -0,0 +1,22 @@
+const reviews = {
+  1: {
+    partOne: [],
+    partTwo: [],
+  },
+  2: {
+    partOne: [],
+    partTwo: [],
+  },
+  3: {
+    partOne: [],
+    partTwo: [],
+  },
+  4: {
+    partOne: [],
+    partTwo: [],
+  },
+  5: {
+    partOne: [],
+    partTwo: [],
+  },
+};
