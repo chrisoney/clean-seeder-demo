f5e48e1b22a58160b5b1847b5a72645d53f4632a
chrisoney <christopher.oney@yahoo.com>
2021-03-05 23:06:19 -0800

    add new review system to recommendation seeding

--- db/seeders/20201127052408-insertRecommendations.js
+++ db/seeders/20201127052408-insertRecommendations.js
@@ -2,6 +2,7 @@
 // tempRecs is just for demo lecture purposes
 const { tempRecs, currentRecs } = require('../../current.js');
 const { addStoryIdToRecs } = require('../../alt_approach.js');
+const reviews = require('../../reviews');
 // importing these so we can query those tables for counts
 const { User, Story } = require('../models')
 
@@ -38,27 +39,38 @@ module.exports = {
     // Making new recommendations with the number of reviews per user,
     // number of users I have, number of stories I have, fake reviews, and id to start at for both users and stories
     function makeRecommendations(
-      numReviews,
-      userEnd,
+      numRecommendations,
+      usersEnd,
       numStories,
       reviews,
-      userStart,
-      storyStart
+      usersStart,
+      storiesStart
     ) {
       // Initializing and empty array to hold them
       const result = [];
       // Looping through all the users except the first one to add reviews for them
-      for (let k = userStart; k < userEnd; k++) {
+      for (let k = usersStart; k < usersEnd; k++) {
         // Keeping track of which stories this user has reviewed
         let stories = [];
-        for (let i = 0; i < numReviews; i++) {
-          // Grabbing a random review index
-          let index = Math.floor(Math.random() * reviews.length);
+        for (let i = 0; i < numRecommendations; i++) {
+          // Randomly picking a rating for the recommendation
+          const rating = Math.floor(Math.random() * 5) + 1;
+
+          // Building the review
+          // Grabbing the array of options for the first half of the review at the level of the randomly generated rating
+          let review = [];
+          Object.values(reviews[rating]).forEach(value => {
+            const arrLength = value.length;
+            const index = Math.floor(Math.random() * arrLength)
+            review.push(value[index]);
+          })
+          review = review.join(' ');
+          
           // Grabbing a random storyId from the ones that are available
-          let storyId = Math.floor(Math.random() * numStories) + storyStart;
+          let storyId = Math.floor(Math.random() * numStories) + storiesStart;
           // Making sure this user hasn't reviewed this story yet
           while (stories.includes(storyId))
-            storyId = Math.floor(Math.random() * numStories) + storyStart;
+            storyId = Math.floor(Math.random() * numStories) + storiesStart;
 
           // Adding the id of the story so we can check against it later on when randomly grabbing a story id
           stories.push(storyId);
@@ -66,9 +78,9 @@ module.exports = {
           // Adding this new review to the results I have collected
           result.push({
             // completely random rating
-            rating: Math.floor(Math.random() * 5) + 1,
+            rating,
             // Grabbing the review with the index we generated a few lines ago
-            review: reviews[index],
+            review,
             userId: k,
             storyId,
           });
@@ -78,51 +90,34 @@ module.exports = {
     }
 
     // Step 2 preparation
-    // Fake reviews
-    const reviews = [
-      'Great!',
-      'Sweet!',
-      '',
-      '',
-      '',
-      'Love it!',
-      'Meh',
-      `From some of my brief discussions with the author, it seemed there were no plans at all to bring this story to the Royalroad platform. Many tried to persuade him with little success. But it seems that Royalroad is going to be graced with this story after all. I have followed this story since sometime back in 2013, and this might be my favourite high-fantasy fiction of all time. `,
-      `One thing I especially love is that the story has a well developed system of rules, and the author actually abides by the rules well enough that I have not spotted any area of cheating. I won't claim to have perfect analysis abilities, but there are exceedingly few stories I can say this about. Usually authors create very loose rules so that this does not become an issue, or they simply break the rules for the sake of plot. Not so in this story. Everything makes sense. This is some of the highest praise I can give to a fantasy story. So many stories that I enjoy suffer from the lack of immersion created by simply not making sense. This story makes sense.`,
-      `Now for my personal reasons for the scores. I find this webnovel amazing, the time loop is executed so well. The MC moves between tasks in a pace that refresh the plot instead of falling into the predicted looping as one might think.`,
-      `I've been following this story for years on fictionpress, so I'm super happy it was finally ported over here. This story is one of the best I've ever encountered online.`,
-      `The characters grow along with the story and the magic system is used in a way befitting a fantasy story, vague at times but detailed to further the story. Not every story must explain in detail how their magic system works.`,
-      `I highly recommend this story to everybody. Even if you don't think you'll like it from what you hear. Not only does this story dodge every single pitfall that comes its way, it absolutely obliterates them.`,
-      `This is absolutely terrible`,
-    ];
     // Querying to figure out how many users I've seeded already
     const numUsers = await User.count();
     // Querying to figure out how many stories I've seeded already
     const numStories = await Story.count();
     // Choosing how many reviews each user should have
-    const numRecs = 5;
+    const numRecs = 20;
     // Finding the first user in my database and grabbing the id, then adding one because I don't want to add reviews to 'chris'
-    let userStart = await User.findOne();
-    userStart = userStart.id + 1;
+    let usersStart = await User.findOne();
+    usersStart = usersStart.id + 1;
     // Finding the first story in my database and grabbing the id
-    let storyStart = await Story.findOne();
-    storyStart = storyStart.id;
+    let storiesStart = await Story.findOne();
+    storiesStart = storiesStart.id;
 
     // Step 2 implementation
     // Creating those new fake recommendations.
     // numRecs is the number of recommendations per user.
     // numUsers and numStories are self explanatory.
-    // Reviews are just a few fake reviews I created/stole from a website (I'm sure you can tell which is which). Fun idea: make different tiers of reviews you can choose from based on a rating!
-    // userStart is to determine the id I would start at
-    // storyStart is to determine the id I would start at
+    // Reviews are pulled from the object that I created in reviews.js
+    // usersStart is to determine the id I would start at
+    // storiesStart is to determine the id I would start at
     let recArray = makeRecommendations(
       numRecs,
       // This is more of a me problem because I artificially inflated the starting point, so I have to correct for that when determining where to stop
-      numUsers + userStart - 1,
+      numUsers + usersStart - 1, // usersEnd
       numStories,
       reviews,
-      userStart,
-      storyStart
+      usersStart,
+      storiesStart
     );
 
     // Step 3
