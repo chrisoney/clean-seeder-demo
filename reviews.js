19d2954d5fbdec51c6e12994f1b1b3ef71f420e0
chrisoney <christopher.oney@yahoo.com>
2021-03-05 20:34:14 -0800

    finished 1 star reviews part one

--- reviews.js
+++ reviews.js
@@ -1,22 +1,82 @@
 const reviews = {
   1: {
-    partOne: [],
-    partTwo: [],
+    partOne: [
+      `I'm not a fan of this story.`,
+      `Started off well, but I ended up disappointed.`,
+      `Total waste of time.`,
+      `I really tried to give this story a fair shot, but at this point I'm giving up.`,
+      `If you're interested in this story, move on to the next one instead.`,
+    ],
+    partTwo: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
   },
   2: {
-    partOne: [],
-    partTwo: [],
+    partOne: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
+    partTwo: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
   },
   3: {
-    partOne: [],
-    partTwo: [],
+    partOne: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
+    partTwo: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
   },
   4: {
-    partOne: [],
-    partTwo: [],
+    partOne: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
+    partTwo: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
   },
   5: {
-    partOne: [],
-    partTwo: [],
+    partOne: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
+    partTwo: [
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+      `sample text`,
+    ],
   },
 };
